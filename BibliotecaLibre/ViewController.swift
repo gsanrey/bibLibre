8635bedcbfea01356171a74ea21878c0c304e809
Gabriel Urso Santana Reyes <briel@macbook-pro-de-gabriel.local>
2015-11-25 16:53:33 +0100

    Biblioteca Libre 2

--- BibliotecaLibre/ViewController.swift
+++ BibliotecaLibre/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  BibliotecaLibre
+//  BibliotecaLibre2
 //
 //  Created by Gabriel Urso Santana Reyes on 22/11/15.
 //  Copyright © 2015 Gabriel Urso Santana Reyes. All rights reserved.
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var isbn: UITextField!
     @IBOutlet weak var texto: UITextView!
     
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var autores: UILabel!
+    @IBOutlet weak var portada: UIImageView!
+    
     @IBAction func buscar(sender: AnyObject) {
-        texto.text = obtenerLibro(isbn.text!) //isbn.text
+        var libro = obtenerLibro(isbn.text!)
+        if libro == nil{
+            let alert = UIAlertController(title: "Error", message: "ISBN no encontrado", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+                switch action.style{
+                case .Default:
+                    print("default")
+                    
+                case .Cancel:
+                    print("cancel")
+                    
+                case .Destructive:
+                    print("destructive")
+                }
+            }))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }else{
+            self.titulo.text = (libro!["titulo"])
+            self.autores.text = (libro!["autores"])
+            let url:NSURL? = NSURL(string: "https://ia700809.us.archive.org/zipview.php?zip=/15/items/olcovers541/olcovers541-L.zip&file=5413481-L.jpg")
+            let data:NSData? = NSData(contentsOfURL : url!)
+            self.portada.image = UIImage(data : data!)
+            
+        }
     }
     
     @IBAction func limpiar(sender: AnyObject) {
@@ -22,18 +50,58 @@ class ViewController: UIViewController {
         
     }
     
-    func obtenerLibro(identificador : String) -> String{
+    func obtenerLibro(identificador : String) -> [String:String]?{
         let destino = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + identificador
         let url = NSURL(string : destino )
         let datos : NSData? = NSData(contentsOfURL: url!)
         if datos == nil{
-            return ("SIN CONEXIÓN")
+            let alert = UIAlertController(title: "Error", message: "Error de conexión", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+                switch action.style{
+                case .Default:
+                    print("default")
+                    
+                case .Cancel:
+                    print("cancel")
+                    
+                case .Destructive:
+                    print("destructive")
+                }
+            }))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            return (nil)
         }
-        var dat = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        let dat = NSString(data: datos!, encoding: NSUTF8StringEncoding)
         if dat! as String == "{}"{
-            dat = "NO ENCONTRADO"
+            return (nil)
         }
-        return dat! as String
+        
+        do{
+            var resultado : [String: String]
+            if datos == nil{
+                return (nil)
+            }else{
+                let json  = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let libro = json as! NSDictionary
+                var autores = ""
+                for i in libro["ISBN:978-84-376-0494-7"]!["authors"]! as! NSArray{
+                    autores = autores + (i["name"] as! String ) + " - "
+                }
+
+                resultado = ["titulo" : libro["ISBN:978-84-376-0494-7"]!["title"]! as! String]
+                resultado["autores"] = autores
+                if libro["ISBN:978-84-376-0494-7"]!["cover"]! == nil{
+                    resultado["portada"] = ""
+                }else{
+                    resultado["portada"] = libro["ISBN:978-84-376-0494-7"]!["cover"]! as! String
+                }
+
+                return resultado
+            }
+        }catch _{
+            return (nil)
+        }
     }
     
     override func viewDidLoad() {
